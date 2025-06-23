1da49f25385265bf03aef22d274a6c111c114932
Lucas Alves <lucasoralves2004@gmail.com>
2025-06-23 16:16:37 -0300

    feat: add localized error messages for 401 and 405 status codes

--- src/main/java/dsw/concessionaria/controller/ErrorViewController.java
+++ src/main/java/dsw/concessionaria/controller/ErrorViewController.java
@@ -18,6 +18,10 @@ public class ErrorViewController implements ErrorViewResolver {
 		ModelAndView model = new ModelAndView("erro");
 		model.addObject("status", status.value());
 		switch (status.value()) {
+		case 401:
+			model.addObject("error", "erro.401.titulo");
+			model.addObject("message", "erro.401.mensagem");
+			break;
 		case 403:
 			model.addObject("error", "erro.403.titulo");
 			model.addObject("message", "erro.403.mensagem");
@@ -26,9 +30,14 @@ public class ErrorViewController implements ErrorViewResolver {
 			model.addObject("error", "erro.404.titulo");
 			model.addObject("message", "erro.404.mensagem");
 			break;
+		case 405:
+			model.addObject("error", "erro.405.titulo");
+			model.addObject("message", "erro.405.mensagem");
+			break;
 		case 500:
 			model.addObject("error", "erro.500.titulo");
 			model.addObject("message", "erro.500.mensagem");
+			break;
 		default:
 			model.addObject("error", "erro.default.titulo");
 			model.addObject("message", "erro.default.mensagem");
